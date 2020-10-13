ed850ec9d0a9839861239359f215dd787cb14fcf
Davidass <gidrig@gmail.com>
2020-10-13 23:10:50 +0300

    add Native lazy-loading - feature detection

--- js/script.js
+++ js/script.js
@@ -50,6 +50,33 @@ refs.ulGallery.insertAdjacentHTML('beforeend', cardsGallery);
 
 refs.ulGallery.addEventListener('click', openGallery);
 
+// Native lazy-loading - feature detection
+
+function addSrcAttrTolazyImages() {
+  const lazyImages = document.querySelectorAll('img[loading="lazy"]');
+  lazyImages.forEach(img => {
+    img.src = img.dataset.src;
+  });
+}
+
+function addlazySizesScript() {
+  const script = document.creatElement('script');
+  script.srs =
+    'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.2.2/lazysizes.min.js';
+  script.integrity =
+    'sha512-TmDwFLhg3UA4ZG0Eb4MIyT1O1Mb+Oww5kFG0uHqXsdbyZz9DcvYQhKpGgNkamAI6h2lGGZq2X8ftOJvF/XjTUg==';
+  script.crossOrigin = 'anonymous';
+  document.body.appendChild(script);
+}
+
+if ('loading' in HTMLIFrameElement.prototype) {
+  // supported in browser
+  addSrcAttrTolazyImages();
+} else {
+  // fetch polyfill/third-party library
+  addlazySizesScript();
+}
+
 // Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
 
 function openGallery(event) {
